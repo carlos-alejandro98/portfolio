4f7c2cd69b79f851f7f5530e6e21976c5c1d0e9f
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2024-02-06 16:53:06 -0300

    avances carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,9 +9,12 @@ export default {
 			},
 			backgroundImage: {
 				'radial-gradient': 'linear-gradient(89.7deg, rgb(0, 0, 0) -10.7%, rgb(53, 92, 125) 88.8%);',
+				
+			},
+			backgroundColor: {
+				'backgroundCarouselBlack': 'rgba(199, 239, 255, 0.1)',
 			},
 		}
 	},
-	
 	plugins: [],
 }
