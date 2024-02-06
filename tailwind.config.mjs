01205136914e947f08cd4dac9b9e73ee3a9a8f5e
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2024-02-06 11:08:00 -0300

    avances tema blanco y negro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				'color-carlos': '#2196f3',
 			},
+			backgroundImage: {
+				'radial-gradient': 'radial-gradient(circle at 10% 20%, rgb(0, 0, 0) 0%, rgb(64, 64, 64) 90.2%)',
+			},
 		}
 	},
+	
 	plugins: [],
 }
