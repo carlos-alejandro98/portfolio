dfff5f6945a3a3b3ffa83f905522e8d939d5ebe7
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2024-05-06 20:35:39 -0400

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   site: 'https://porfolio.dev/'
-})
+});
